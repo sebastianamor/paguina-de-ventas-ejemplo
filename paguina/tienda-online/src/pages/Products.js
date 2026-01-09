6dfa4a6a8f76205fbb7bae2518c07e0492cd12f0
¨Jane <smoreira11@hotmail.com>
2026-01-09 22:34:02 +0900

    add

--- paguina/tienda-online/src/pages/Products.js
+++ paguina/tienda-online/src/pages/Products.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import ProductCard from '../components/products/ProductCard';
+import ProductCard from '../components/orders/OrderCard';
 import './products.css';
 
 const Products = () => {
