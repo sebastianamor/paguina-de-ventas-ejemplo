23fbd2948888b11d8a2b0d9ccc75541eb3e5dc19
¨Jane <smoreira11@hotmail.com>
2026-01-09 22:20:33 +0900

    add

--- paguina/tienda-online/src/pages/Products.js
+++ paguina/tienda-online/src/pages/Products.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ProductCard from '../components/products/ProductCard';
-import './Products.css';
+import './products.css';
 
 const Products = () => {
   const [products, setProducts] = useState([]);
