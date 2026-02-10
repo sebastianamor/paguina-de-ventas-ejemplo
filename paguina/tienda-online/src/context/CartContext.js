5f84b7c6b5e3ea3fce25374f6b54b6587eedf3b4
¨Jane <smoreira11@hotmail.com>
2026-02-10 16:30:31 +0900

    add

--- paguina/tienda-online/src/context/CartContext.js
+++ paguina/tienda-online/src/context/CartContext.js
@@ -6,27 +6,11 @@ export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-    setCart(prev => {
-      const exists = prev.find(p => p.id === product.id);
-
-      if (exists) {
-        return prev.map(p =>
-          p.id === product.id
-            ? { ...p, quantity: p.quantity + 1 }
-            : p
-        );
-      }
-
-      return [...prev, { ...product, quantity: 1 }];
-    });
-  };
-
-  const removeFromCart = (id) => {
-    setCart(prev => prev.filter(p => p.id !== id));
+    setCart((prev) => [...prev, product]);
   };
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+    <CartContext.Provider value={{ cart, addToCart }}>
       {children}
     </CartContext.Provider>
   );
